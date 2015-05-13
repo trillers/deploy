8d30e0b1bf1b2e20d600a1fd7e5ef07f1bd9ff48
henryleu <henryleu@126.com>
2015-05-13 12:11:03 +0800

    add token

--- deploy-hawaii-ci.sh
+++ deploy-hawaii-ci.sh
@@ -2,7 +2,7 @@ cd /home/www/hawaii/
 git pull
 
 npm install
-npm install seedtrip/hawaii-settings#ci
+npm install https://seedtripgit:d6fdc047f748aa51ed3e13639eeffae0da9cb1db@github.com/seedtrip/hawaii.git#ci
 
 cd /home/www/deploy
 pm2 reload hawaii
