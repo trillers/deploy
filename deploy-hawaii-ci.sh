d6eb295cd382191655b2b938da35b702f6a6e465
henryleu <henryleu@126.com>
2015-05-18 14:53:21 +0800

    pull origin develop

--- deploy-hawaii-ci.sh
+++ deploy-hawaii-ci.sh
@@ -1,5 +1,5 @@
 cd /home/www/hawaii/
-git pull
+git pull origin develop
 
 npm install
 npm install https://seedtripgit:d6fdc047f748aa51ed3e13639eeffae0da9cb1db@github.com/seedtrip/hawaii-settings.git#ci
