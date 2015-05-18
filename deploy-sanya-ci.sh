4079053120169b8b140a2b193ca8d64e764d8f15
tianj0522 <451587411@qq.com>
2015-05-18 16:14:29 +0800

    pull origin develop

--- deploy-sanya-ci.sh
+++ deploy-sanya-ci.sh
@@ -1,5 +1,5 @@
 cd /home/www/sanya/
-git pull
+git pull origin  develop
 
 npm install
 npm install https://seedtripgit:d6fdc047f748aa51ed3e13639eeffae0da9cb1db@github.com/seedtrip/sanya-settings.git#ci
