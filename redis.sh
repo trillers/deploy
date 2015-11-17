874f87ad743264096aced191c70ce2775ef86628
henryleu <henryleu@126.com>
2015-11-17 09:54:42 +0800

    add redis conf files

--- redis.sh
+++ redis.sh
@@ -1,10 +1,10 @@
 ## init redis config file for a redis instance
-mkdir /etc/redis
-cp redis.conf /etc/redis/6379.conf
+sudo mkdir /etc/redis
+sudo cp redis.conf /etc/redis/6379.conf
 ## create redis data file for a redis instance
-mkdir /var/redis
-mkdir /var/redis/6379
+sudo mkdir /var/redis
+sudo mkdir /var/redis/6379
 ## setup init script for a redis instance
-cp utils/redis_init_script /etc/init.d/redis_6379
-update-rc.d redis_6379 defaults
+sudo cp utils/redis_init_script /etc/init.d/redis_6379
+sudo update-rc.d redis_6379 defaults
 
