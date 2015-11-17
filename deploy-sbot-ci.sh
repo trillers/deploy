ed9a568f617402abeb47fa0ac619dfefb34cac38
henryleu <henryleu@126.com>
2015-11-17 14:20:35 +0800

    add ci and prd version of nginx

--- deploy-sbot-ci.sh
+++ deploy-sbot-ci.sh
@@ -1,6 +1,6 @@
 cd /home/www/sbot/
-git checkout develop
-git pull origin develop
+git checkout master
+git pull origin master
 
 npm install
 npm install https://henryleu:c148960791ab761064ecfbb07b7fdf582ff008df@github.com/trillers/athena-settings.git#ci
