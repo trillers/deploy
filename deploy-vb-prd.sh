745dc123e99b10478426402f3979b4c2184cf85b
leekangtaqi <leekangtaqi@sina.com>
2015-12-31 11:22:22 +0800

    add prd deploy

---
+++ deploy-vb-prd.sh
@@ -0,0 +1,9 @@
+cd /home/www/vb/
+git checkout master
+git pull origin master
+
+npm install
+npm install https://henryleu:c148960791ab761064ecfbb07b7fdf582ff008df@github.com/trillers/base-settings.git#prd
+
+cd /home/www/deploy
+pm2 reload vb
