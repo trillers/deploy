bd697777ecd4f9d2f75cdf5e9ae10ef5867c3683
henryleu <henryleu@126.com>
2016-01-26 12:20:01 +0800

    update note config

--- deploy-note-prd.sh
+++ deploy-note-prd.sh
@@ -1,9 +1,9 @@
-cd /home/www/vmark/
-git checkout master
-git pull origin master
+cd /home/www/note/
+git checkout membook
+git pull origin membook
 
 npm install
-npm install https://henryleu:c148960791ab761064ecfbb07b7fdf582ff008df@github.com/trillers/base-settings.git#prd
+npm install https://henryleu:c148960791ab761064ecfbb07b7fdf582ff008df@github.com/trillers/base-settings.git#nprd
 npm install https://henryleu:c148960791ab761064ecfbb07b7fdf582ff008df@github.com/trillers/vmark-settings.git#nprd
 
 cd /home/www/deploy
